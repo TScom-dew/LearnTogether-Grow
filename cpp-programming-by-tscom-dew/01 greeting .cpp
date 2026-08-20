9321b6ea5105809a1ca79edc274eabb6cc673790
Ashlok Kumar <ashlokkumar2006@gmail.com>
2026-08-21 03:47:36 +0530

    Add Hello World program in C++

---
+++ cpp-programming-by-tscom-dew/01 greeting .cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main(){
+    // for fast input output
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    // printing Hello, World
+    std::cout << "Hello, World" << std::endl;
+    
+    return 0;
+}
